e0ab85f07fc92fdb2b4d2836095c9ab710e042bf
w-wonk <w-wonk@yandex.ru>
2016-09-29 14:20:17 +0300

    added webpack configuration and simple component

---
+++ app/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from 'react-dom';
+import Greeting from "./components/greeting";
+
+ReactDOM.render(
+  <Greeting name="World"/>, document.getElementById("content")
+);
