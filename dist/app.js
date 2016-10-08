15ddbefabdf773272147514d648f571ad271bc9f
w-wonk <w-wonk@yandex.ru>
2016-10-08 15:49:52 +0300

    added /logs dirrectory

--- dist/app.js
+++ dist/app.js
@@ -2353,7 +2353,7 @@
 	  for (var propName in typeDef) {
 	    if (typeDef.hasOwnProperty(propName)) {
 	      // use a warning instead of an invariant so components
-	      // don't show up in prod but only in __DEV__
+	      // don't show up in uc but only in __DEV__
 	      process.env.NODE_ENV !== 'production' ? warning(typeof typeDef[propName] === 'function', '%s: %s type `%s` is invalid; it must be a function, usually from ' + 'React.PropTypes.', Constructor.displayName || 'ReactClass', ReactPropTypeLocationNames[location], propName) : void 0;
 	    }
 	  }
